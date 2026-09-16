a03b2b77bc2f96493606733b80b405afd75d1632
otito <otitoamuga@gmail.com>
2026-09-15 20:07:00 -0600

    j

---
+++ divisibility.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void count_divisible_elements(vector<int> a) {
+  for (int num : a) {
+    cout << "Divisible by " << num << endl;
+    for (int i = 1; i <= 1689; i++) {
+      if (i % num == 0) {
+        cout << "\t\t" << i << endl;
+      }
+    }
+  }
+}
+
+int main() {
+
+  vector<int> a {3, 5, 11, 15, 165, 55};
+  count_divisible_elements(a);
+}
