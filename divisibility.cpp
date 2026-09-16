c445eced508457055592a07aabbe00e57157a4a7
otito <otitoamuga@gmail.com>
2026-09-15 20:09:54 -0600

    k

--- divisibility.cpp
+++ divisibility.cpp
@@ -4,12 +4,15 @@ using namespace std;
 
 void count_divisible_elements(vector<int> a) {
   for (int num : a) {
+    int res = 0;
     cout << "Divisible by " << num << endl;
     for (int i = 1; i <= 1689; i++) {
       if (i % num == 0) {
+        res++;
         cout << "\t\t" << i << endl;
       }
     }
+    cout << "\tcardinality/count: " << res << endl;
   }
 }
 
