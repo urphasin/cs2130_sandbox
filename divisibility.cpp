a9be1e1181e8e2ab5183efcab82293f5ce0675ce
otito <otitoamuga@gmail.com>
2026-09-15 21:19:24 -0600

    push

--- divisibility.cpp
+++ divisibility.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 // U = {1, 2, 3, ... , 1689}
 // how many??? {x:x ∈ U && 3|x}
+// (A ∪ B ∪ C)???  i % 3 == 0 || i % 5 == 0 || i % 11 == 0
 void count_divisible_elements(vector<int> a) {
   for (int num : a) {
     int res = 0;
@@ -19,6 +20,6 @@ void count_divisible_elements(vector<int> a) {
 
 int main() {
 
-  vector<int> a {3, 5, 11, 15, 165, 55};
+  vector<int> a {3}; //{3, 5, 11, 15, 165, 55}
   count_divisible_elements(a);
 }
